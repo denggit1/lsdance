103ff2ccee3600261df60031648e71a08a75fc8e
denggit1 <35787501@qq.com>
2018-12-06 19:23:18 +0800

    v2.1

--- manage.py
+++ manage.py
@@ -1,9 +1,20 @@
 # coding=utf-8
 from flask import Flask, render_template
 from flask_script import Manager
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, EqualTo
 
 app = Flask(__name__)
 manage = Manager(app)
+# en64=base64.b64encode(uuid.uuid4().bytes)
+app.config['SECRET_KEY'] = 'XL57Byw6Tj2q3U7gtDWDIw=='
+
+
+class RegistForm(FlaskForm):
+    uname = StringField(label=u'学号', validators=[DataRequired(u'学号不能为空')])
+    upwd = PasswordField(label=u'密码', validators=[DataRequired(u'密码不能为空')])
+    upwd2 = PasswordField(label=u'密码2', validators=[DataRequired(u'密码不能为空'), EqualTo('upwd', u'两次密码输入不一致')])
 
 
 @app.route('/')
@@ -34,6 +45,18 @@ def dancemv():
 @app.route('/dancemusic')
 def dancemusic():
     return render_template('dancemusic.html')
+
+
+@app.route('/login')
+def login():
+    form = RegistForm()
+    return render_template('login.html', form=form)
+
+
+@app.route('/register')
+def register():
+    form = RegistForm()
+    return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,19 @@
 $(function () {
     $(".lb").css({"width":$(".img").css("width")});
+    $(".hh").css({"width":$(window).width()/2});
+    $(".sr").children("div").children("input").addClass("form-control");
     $(window).resize(function() {
       $(".lb").css({"width":$(".img").css("width")});
+      $(".hh").css({"width":$(window).width()/2});
     });
+    $(".zctj").click(function() {
+        var pwd = $("input[name='upwd']").val();
+        var pwd2 = $("input[name='upwd2']").val();
+        if (pwd != pwd2) {
+            alert("两次密码不一致!");
+            $("input[name='upwd']").val("");
+            $("input[name='upwd2']").val("");
+            return false;
+        }
+    })
 });
