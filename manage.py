ffee05d5cb888a581f99cfc7225a2d4418607303
denggit1 <35787501@qq.com>
2018-12-07 18:34:34 +0800

    v2.1.2

--- manage.py
+++ manage.py
@@ -2,10 +2,11 @@
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import IntegerField, PasswordField
 from wtforms.validators import DataRequired, EqualTo
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
+from sha import sha
 
 app = Flask(__name__)
 manage = Manager(app)
@@ -19,9 +20,35 @@ manage.add_command("db", MigrateCommand)
 
 
 class RegistForm(FlaskForm):
-    uname = StringField(label=u'学号', validators=[DataRequired(u'学号不能为空')])
+    stuid = IntegerField(label=u'学号', validators=[DataRequired(u'学号不能为空')])
     upwd = PasswordField(label=u'密码', validators=[DataRequired(u'密码不能为空')])
     upwd2 = PasswordField(label=u'密码2', validators=[DataRequired(u'密码不能为空'), EqualTo('upwd', u'两次密码输入不一致')])
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    stuid = db.Column(db.Integer, unique=True, nullable=False)
+    pwd = db.Column(db.String(40), unique=False, nullable=False)
+    name = db.Column(db.String(32), unique=False, nullable=True)
+    phone = db.Column(db.Integer, unique=False, nullable=True)
+    class_id = db.Column(db.Integer, db.ForeignKey('class.id'), default=1)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'), default=1)
+
+
+class Class(db.Model):
+    __tablename__ = 'class'
+    id = db.Column(db.Integer, primary_key=True)
+    Class = db.Column(db.String(32), unique=True, nullable=True)
+    college = db.Column(db.String(32), unique=False, nullable=True)
+    users = db.relationship("User", backref="class")
+
+
+class Role(db.Model):
+    __tablename__ = 'role'
+    id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String(32), unique=False, nullable=False)
+    users = db.relationship("User", backref="role")
 
 
 @app.route('/')
@@ -54,15 +81,33 @@ def dancemusic():
     return render_template('dancemusic.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     form = RegistForm()
+    print('login')
+    if form.validate_on_submit():
+        print('hehe')
+        stuid = form.stuid.data
+        upwd = form.upwd.data
+        s1 = sha()
+        s1.update(upwd)
+        pwd = s1.hexdigest()
+        print(db.session.query(User).filter(User.stuid == stuid).count())
     return render_template('login.html', form=form)
 
 
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register():
     form = RegistForm()
+    if form.validate_on_submit():
+        stuid = form.stuid.data
+        upwd = form.upwd.data
+        s1 = sha()
+        s1.update(upwd)
+        pwd = s1.hexdigest()
+        user1 = User(stuid=int(stuid), pwd=pwd)
+        db.session.add(user1)
+        db.session.commit()
     return render_template('register.html', form=form)
 
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,25 @@
 $(function () {
     $(".lb").css({"width":$(".img").css("width")});
-    $(".hh").css({"width":$(window).width()/2});
+    // $(".hh").css({"width":$(window).width()/2});
     $(".sr").children("div").children("input").addClass("form-control");
     $(window).resize(function() {
       $(".lb").css({"width":$(".img").css("width")});
-      $(".hh").css({"width":$(window).width()/2});
+      // $(".hh").css({"width":$(window).width()/2});
     });
     $(".zctj").click(function() {
+        var stuid = $.trim($("input[name='stuid']").val());
         var pwd = $("input[name='upwd']").val();
         var pwd2 = $("input[name='upwd2']").val();
-        if (pwd != pwd2) {
+        var reg1 = /^[1]\d[2-3][0]\d{5}$/;   /*定义验证表达式*/
+        if(!reg1.test(stuid)){
+            alert("请输入规范的学号!");
+            return false;
+        }
+        else if(pwd.length<6 | pwd.length>16){
+            alert("请输入6-16位的密码!");
+            return false;
+        }
+        else if (pwd != pwd2) {
             alert("两次密码不一致!");
             $("input[name='upwd']").val("");
             $("input[name='upwd2']").val("");
